aedcdb8f873c91f5f6ad12ba54259264a555bd09
nick <40447503+nickstanley574@users.noreply.github.com>
2024-11-16 12:49:30 -0600

    Add logic to process captive whale key attributes

--- killer_whale_fandom_com_scrapper.py
+++ killer_whale_fandom_com_scrapper.py
@@ -150,7 +150,7 @@ def get_all_items():
 
     return items
 
-# filename = "killerwhales_fandom.json"
+filename = "killerwhales_fandom.json"
 
 # with open(filename, "w") as fp:
 #     json.dump(get_all_items(), fp, sort_keys=True, indent=4) 
@@ -163,10 +163,10 @@ def was_captured(data):
     return 'place_of_capture' in data
 
 def born_in_captivity(data):
-    return 'place_of_birth' in data
+    return 'place_of_birth' in data and 'latest_sighting' not in data
 
-
-
+def is_captive_whale(data):
+    return is_killer_whale(data) and (born_in_captivity(data) or was_captured(data))
 
 
 
@@ -174,8 +174,35 @@ def born_in_captivity(data):
 with open(filename, 'r') as file:
     data = json.load(file)
 
-for i in data:
-    if 'Title' in i:
-        print(f"{i['Title']} - {is_killer_whale(i)}")
-    else:
-        print(f"{i['title1']} - {is_killer_whale(i)}")
+for item in data:
+
+    if is_captive_whale(item):
+
+        name = item['title1'].split('-')[0].strip()
+        sex = item['sex']
+
+        mother = 'Unknown'
+        if "mother" in item:
+            mother = item['mother']
+
+        father = 'Unknown'
+        if "father" in item:
+            father = item['father']
+
+        url = item['url']
+
+        born_or_caught = "Born"
+        if (was_captured(item)):
+            born_or_caught = 'Caught'
+
+        status = 'Undefined'
+        if 'status' in item:
+            status = item['status']
+        elif 'place_of_death' in item:
+            status = 'Deceased'
+        elif 'date_of_release' in item:
+            status = "Released"
+
+
+        print(f"{name:<28} {sex:<11} {born_or_caught:<8} {mother:<20} {father:<20} {status:<10} {url}")
+
