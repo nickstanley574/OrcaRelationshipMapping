27a38c22d66d640b4050750913d48a9185b39dcf
nick <40447503+nickstanley574@users.noreply.github.com>
2024-11-16 12:59:19 -0600

    Split scrapper and processing into seprate scripts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # OrcaRelationshipMapping
+
+
+
+## General Notes 
+
+- Exluding accidently captures i.e. fish nets 
+
+- https://www.youtube.com/watch?v=TnJVE2oNJH0
+
+- https://stackoverflow.com/questions/53151296/family-tree-with-pure-html-and-css-or-with-minimal-js (Maybe use?)
--- scrapper.py
+++ scrapper.py
@@ -152,57 +152,7 @@ def get_all_items():
 
 filename = "killerwhales_fandom.json"
 
-# with open(filename, "w") as fp:
-#     json.dump(get_all_items(), fp, sort_keys=True, indent=4) 
+with open(filename, "w") as fp:
+    json.dump(get_all_items(), fp, sort_keys=True, indent=4) 
 
-
-def is_killer_whale(data):
-    return 'sex' in data
-
-def was_captured(data):
-    return 'place_of_capture' in data
-
-def born_in_captivity(data):
-    return 'place_of_birth' in data and 'latest_sighting' not in data
-
-def is_captive_whale(data):
-    return is_killer_whale(data) and (born_in_captivity(data) or was_captured(data))
-
-
-
-# Open and read the JSON file
-with open(filename, 'r') as file:
-    data = json.load(file)
-
-for item in data:
-
-    if is_captive_whale(item):
-
-        name = item['title1'].split('-')[0].strip()
-        sex = item['sex']
-
-        mother = 'Unknown'
-        if "mother" in item:
-            mother = item['mother']
-
-        father = 'Unknown'
-        if "father" in item:
-            father = item['father']
-
-        url = item['url']
-
-        born_or_caught = "Born"
-        if (was_captured(item)):
-            born_or_caught = 'Caught'
-
-        status = 'Undefined'
-        if 'status' in item:
-            status = item['status']
-        elif 'place_of_death' in item:
-            status = 'Deceased'
-        elif 'date_of_release' in item:
-            status = "Released"
-
-
-        print(f"{name:<28} {sex:<11} {born_or_caught:<8} {mother:<20} {father:<20} {status:<10} {url}")
 
